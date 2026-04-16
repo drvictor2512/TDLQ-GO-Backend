788e1b599f1a3b4337587806875dfa99687050b4
ChinhLe <thongchinhle.12c2@gmail.com>
2026-04-16 11:35:49 +0700

    update service user

--- TLDQ-GO-USER-SERVICE/config/db.js
+++ TLDQ-GO-USER-SERVICE/config/db.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.USER_SERVICE_MONGO_URI, {
-      dbName: process.env.USER_DB_NAME,
-    });
+    const uri = process.env.MONGO_URI || process.env.USER_SERVICE_MONGO_URI || process.env.MONGO_URL;
+    const dbName = process.env.DB_NAME || process.env.USER_DB_NAME || process.env.USER_SERVICE_DB_NAME;
 
+    if (!uri) {
+      console.error('MongoDB URI is not set. Please set MONGO_URI or USER_SERVICE_MONGO_URI in environment');
+      process.exit(1);
+    }
+
+    await mongoose.connect(uri, {
+      dbName,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error);
