26cf875f25735b4d2000131baab1b1fcf78581f4
Tran Thang <trvictor2512@gmail.com>
2026-04-29 17:30:26 +0700

    Thêm remove, update voucher

--- TLDQ-GO-PRODUCT-SERVICE/routes/voucher.routes.js
+++ TLDQ-GO-PRODUCT-SERVICE/routes/voucher.routes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const {
   createVoucher,
   getVouchersBySeller,
+  updateVoucher,
+  deleteVoucher,
 } = require("../controllers/voucher.controller");
 
 router.post("/vouchers", createVoucher);
+router.put("/vouchers/:id", updateVoucher);
+router.delete("/vouchers/:id", deleteVoucher);
 router.get("/vouchers/seller/:seller_id", getVouchersBySeller);
 
 module.exports = router;
